fa8bf6966a73e7ec48fe100605413dba20f10a09
paulomiim <paulomimehta0208@yahoo.com>
2024-09-09 21:07:08 -0700

    Update String1.java

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -279,8 +279,11 @@ public class String1
      */
     public String conCat(String a, String b) {
         String output = "";
-        
-        if (a.substring(a.length()-1, a.length()).equals(b.substring(0,1))){
+        if (a.length() < 1){
+            return b;
+        }else if(b.length() < 1){
+            return a;
+        }else if (a.substring(a.length()-1, a.length()).equals(b.substring(0,1))){
             output = a.substring(0, a.length()-1) + b; 
             return output;
         }else{
